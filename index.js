1e775e5d1d6bb85cd073c2675c2877a43c88d786
Tarun <tarun@gmail.com>
2024-12-18 21:28:40 +0530

    commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Role display function
 let assign = [
     {h_b : "Web Developer"},
     {h_b : "Graphic Designer"},
@@ -22,7 +23,8 @@ setInterval(()=>{update()},4000);
 
 update()
 
-
+//Product display function
+const cardContainer = document.querySelector('.port2'); 
 var products = [
     { img: "./IMAGES/p.jpg" },
     { img: "./IMAGES/pro4.png" },
@@ -31,10 +33,9 @@ var products = [
     { img: "./IMAGES/pro1.png" },
     { img: "./IMAGES/pg1.jpg" },
     { img: "./IMAGES/pro3.png" },
-    { img: "./IMAGES/pg2.jpg" },
+    { img: "./IMAGES/pg2.jpg" }
 ];
 
-const cardContainer = document.querySelector('.port2'); 
 if (cardContainer) { 
     products.forEach(product => {
         const card = document.createElement('div');
@@ -43,3 +44,93 @@ if (cardContainer) {
         cardContainer.appendChild(card);
     });
 }
+
+const all_btn = document.querySelector('#all');
+all_btn.addEventListener('click',() =>{
+
+    setTimeout(() => {
+        let products = [
+            { img: "./IMAGES/p.jpg" },
+            { img: "./IMAGES/pro4.png" },
+            { img: "./IMAGES/pg.jpg" },
+            { img: "./IMAGES/pro2.png" },
+            { img: "./IMAGES/pro1.png" },
+            { img: "./IMAGES/pg1.jpg" },
+            { img: "./IMAGES/pro3.png" },
+            { img: "./IMAGES/pg2.jpg" }
+        ];
+        
+        if (cardContainer) { 
+            products.forEach(product => {
+                const card = document.createElement('div');
+                card.className = "card";
+                card.innerHTML = `<img src="${product.img}" alt="ProductPics" class="card_img">`;
+                cardContainer.appendChild(card);
+            });
+        }
+    },1000);
+});
+
+const ps_btn = document.querySelector('#PS');
+ps_btn.addEventListener('click',() =>{
+
+    setTimeout(() => {
+        var ps = [
+            { img: "./IMAGES/p.jpg" },
+            { img: "./IMAGES/pg.jpg" },
+            { img: "./IMAGES/pg1.jpg" },
+            { img: "./IMAGES/pro3.png" }
+        ];
+        
+        if (cardContainer) { 
+            ps.forEach(product => {
+                const card = document.createElement('div');
+                card.className = "card";
+                card.innerHTML = `<img src="${product.img}" alt="ProductPics" class="card_img">`;
+                cardContainer.appendChild(card);
+            });
+        }
+    },1000);
+});
+
+const wd_btn = document.querySelector('#WD');
+wd_btn.addEventListener('click',() =>{
+    setTimeout(() =>{
+        var products = [
+            { img: "./IMAGES/pro4.png" },
+            { img: "./IMAGES/pro2.png" },
+            { img: "./IMAGES/pro1.png" },
+            { img: "./IMAGES/pro3.png" }
+        ];
+
+        if (cardContainer) { 
+            products.forEach(product => {
+                const card = document.createElement('div');
+                card.className = "card";
+                card.innerHTML = `<img src="${product.img}" alt="ProductPics" class="card_img">`;
+                cardContainer.appendChild(card);
+            });
+        }
+    },1000);
+});
+
+const gd_btn = document.querySelector('#GD');
+gd_btn.addEventListener('click',() =>{
+    setTimeout(() =>{
+        var products = [
+            { img: "./IMAGES/p.jpg" },
+            { img: "./IMAGES/pro4.png" },
+            { img: "./IMAGES/pg.jpg" },
+            { img: "./IMAGES/pro2.png" }
+        ];
+
+        if (cardContainer) { 
+            products.forEach(product => {
+                const card = document.createElement('div');
+                card.className = "card";
+                card.innerHTML = `<img src="${product.img}" alt="ProductPics" class="card_img">`;
+                cardContainer.appendChild(card);
+            });
+        }
+    },1000);
+});
